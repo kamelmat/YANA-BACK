f2af3913157391fcd17642e2100f902b17ab051f
Excelsior <ericchristensen59@gmail.com>
2025-04-05 16:03:59 -0300

    feat: se agrega icono svg a cada emotion desde media/emotions

--- yana/apps/emotions/models.py
+++ yana/apps/emotions/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 
 class Emotion(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
--- yana/apps/emotions/serializers.py
+++ yana/apps/emotions/serializers.py
@@ -2,9 +2,14 @@ from rest_framework import serializers
 from .models import *
 
 class EmotionSerializer(serializers.ModelSerializer):
+    icon_url = serializers.SerializerMethodField()
+
     class Meta:
         model = Emotion
-        fields = ['id', 'name']
+        fields = ['id', 'name', 'icon_url']
+
+    def get_icon_url(self, obj):
+        return f"/media/emotions/{obj.name}.svg"
 
 class UserEmotionSerializer(serializers.ModelSerializer):
     class Meta:
