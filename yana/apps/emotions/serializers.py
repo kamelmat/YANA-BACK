e802c64d396598c8e194e8cfc6b26ce550d9eb61
KiraDragoness <patry37.zgz@gmail.com>
2025-04-07 18:24:07 +0200

    feat: extra space

--- yana/apps/emotions/serializers.py
+++ yana/apps/emotions/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import *
 
+
 class EmotionSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(required=False, allow_null=True)
     class Meta:
