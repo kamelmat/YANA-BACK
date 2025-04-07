91a94331c7458bb8e92c45b07668871112ad2769
Blondy Martinez <blondymartinezm@gmail.com>
2025-04-07 21:58:21 +0200

    feat: add user detail endpoint

--- yana/apps/users/urls.py
+++ yana/apps/users/urls.py
@@ -4,8 +4,6 @@ from apps.users.views import UserAPIVew
 from apps.users.views import *
 from rest_framework_simplejwt.views import TokenRefreshView
 
-
-
 urlpatterns = [
     path('api/register/', RegisterView.as_view(), name='register'),
     path('api/login/', LoginView.as_view(), name='login'),
@@ -13,4 +11,5 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/generate-user-id/', GenerateUserIDView.as_view(), name='generate_user_id'),
     path('api/logout/', LogoutView.as_view(), name='logout'),
+    path('api/users/<str:user_id>/', UserDetailView.as_view(), name='user-detail'),
 ]
