6224bf00e28ad8cf42c6a49136d52acb097ec68b
Blondy Martinez <blondymartinezm@gmail.com>
2025-04-09 17:48:24 +0200

    fix: revert last merge

--- yana/apps/maps/models.py
+++ yana/apps/maps/models.py
@@ -1,14 +1,1 @@
 from django.db import models
-from django.conf import settings
-from apps.emotions.models import Emotion
-
-class SharedEmotion(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    emotion = models.ForeignKey(Emotion, on_delete=models.CASCADE)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    is_active = models.BooleanField(default=True)
-
-    def __str__(self):
-        return f"{self.user.user_id} expresó {self.emotion.name} en ({self.latitude}, {self.longitude})"
