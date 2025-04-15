78e99f7706652fdeccd8bb583afbe3103ee7bc0a
KiraDragoness <patry37.zgz@gmail.com>
2025-04-15 23:38:20 +0200

    feat: endpoints for sending support messages and viewing received messages
    -Also added the message package to the site urls

--- yana/apps/message/urls.py
+++ yana/apps/message/urls.py
@@ -1,7 +1,9 @@
 # apps/message/urls.py
 from django.urls import path
-from .views import SupportTemplatesView
+from .views import *
 
 urlpatterns = [
     path('templates/', SupportTemplatesView.as_view(), name='support-templates'),
+    path('api/send-support/', SendSupportMessageView.as_view(), name='send-support'),
+    path('api/received-messages/', ReceivedSupportMessagesView.as_view(), name='received-messages')
 ]
