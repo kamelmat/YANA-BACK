7827c154fc31c8728a281db15ad2db214897d7e4
Excelsior <ericchristensen59@gmail.com>
2025-04-15 11:29:36 -0300

    feat: se actualiza json para messages

--- yana/apps/message/views.py
+++ yana/apps/message/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import SupportMessageTemplate
 
-# Create your views here.
+class SupportTemplatesView(APIView):
+    def get(self, request):
+        templates = SupportMessageTemplate.objects.all()
+        return Response([{'id': t.id, 'text': t.text} for t in templates], status=status.HTTP_200_OK)
