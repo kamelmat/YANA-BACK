4c9e0b0c1cbd3f1c939bb11d1118a85a492fed02
Excelsior <ericchristensen59@gmail.com>
2025-04-15 11:29:36 -0300

    feat: se actualiza json para messages

--- yana/apps/message/models.py
+++ yana/apps/message/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from django.conf import settings
 
+class SupportMessageTemplate(models.Model):
+    text = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.text
+
 class SupportMessage(models.Model):
     sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sent_messages')
     receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='received_messages')
-    message = models.CharField(
-        max_length=100,
-        default="You are not alone, I’m here, I feel you",
-        editable=False
-    )
+    template = models.ForeignKey(SupportMessageTemplate, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
