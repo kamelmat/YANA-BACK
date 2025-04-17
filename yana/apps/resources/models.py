618ec6a66a32d674960d7d83c4c02625a98d25c6
Monique Latil <moninilatil@gmail.com>
2025-04-17 16:20:54 +0200

    fix: "Modelo arreglado, campos nullables"

--- yana/apps/resources/models.py
+++ yana/apps/resources/models.py
@@ -8,7 +8,8 @@ class HelpResource(models.Model):
     url = models.URLField()
     location = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
-    phone = models.CharField(max_length=20, blank=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
 
     def __str__(self):
         return self.name
