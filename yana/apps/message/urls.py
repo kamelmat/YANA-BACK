11d508ad236348a047726185d5e8c10800bd090a
Miguel Cabrera <miguelecabrerav@gmail.com>
2025-04-17 08:54:03 -0400

    feat: message notification

--- yana/apps/message/urls.py
+++ yana/apps/message/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     path('templates/', SupportTemplatesView.as_view(), name='support-templates'),
     path('api/send-support/', SendSupportMessageView.as_view(), name='send-support'),
-    path('api/received-messages/', ReceivedSupportMessagesView.as_view(), name='received-messages')
+    path('api/received-messages/', ReceivedSupportMessagesView.as_view(), name='received-messages'),
+    path('api/notifications/', NotificationsView.as_view(), name='notifications'),
 ]
--- yana/apps/message/views.py
+++ yana/apps/message/views.py
@@ -37,6 +37,9 @@ class SendSupportMessageView(generics.CreateAPIView):
             template=template
         )
 
+        receiver.has_unread_messages = True
+        receiver.save()
+
         return Response({"message": "Mensaje de apoyo enviado"}, status=status.HTTP_201_CREATED)
     
 class ReceivedSupportMessagesView(generics.ListAPIView):
@@ -45,3 +48,10 @@ class ReceivedSupportMessagesView(generics.ListAPIView):
 
     def get_queryset(self):
          return SupportMessage.objects.filter(receiver=self.request.user).order_by('-created_at')
+    
+class NotificationsView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response(user.has_unread_messages, status=status.HTTP_200_OK)
