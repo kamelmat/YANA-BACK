41eb485a47136a9fde7de60d15e315b77a13e51c
Adrián <adrianjoser20@gmail.com>
2025-04-20 18:09:04 -0400

    fix: add access-logfile option to start.py and clean up unused Docker images in backend.yaml

--- .github/scripts/start.py
+++ .github/scripts/start.py
@@ -2,5 +2,5 @@ import sys
 import gunicorn.app.wsgiapp as app
 
 if __name__ == "__main__":
-    sys.argv[1:] = ["--workers", "1", "--threads", "2", "--timeout", "60", "yana.site_app.wsgi:application"]
+    sys.argv[1:] = ["--workers", "1", "--threads", "2", "--timeout", "60", "--access-logfile", "-", "yana.site_app.wsgi:application"]
     app.run()
