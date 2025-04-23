c92f8fff00cbce163c4feca3f18f8918f1f66b47
Adrián <adrianjoser20@gmail.com>
2025-04-22 20:28:28 -0400

    fix: add duplicate entry for '34.95.113.90' in ALLOWED_HOSTS

--- yana/site_app/settings/production.py
+++ yana/site_app/settings/production.py
@@ -26,7 +26,8 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     '10.128.0.2',
     '34.8.224.201',
-    'youare-notalone.duckdns.org'
+    'youare-notalone.duckdns.org',
+    '34.95.113.90'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
