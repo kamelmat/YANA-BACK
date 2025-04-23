f7a1dd004611a1c09ec1c14402ea29f6500a2612
Monique Latil <moninilatil@gmail.com>
2025-04-23 18:54:53 +0200

    fix: organized urls in messages folder

--- yana/apps/message/urls.py
+++ yana/apps/message/urls.py
@@ -3,11 +3,17 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('templates/', SupportTemplatesView.as_view(), name='support-templates'),
-    path('api/send-support/', SendSupportMessageView.as_view(), name='send-support'),
-    path('api/received-messages/', ReceivedSupportMessagesView.as_view(), name='received-messages'),
+    # Admin views
     path('admin/templates/create/', CreateSupportTemplateView.as_view(), name='create-template'),
     path('admin/templates/delete/<int:pk>/', DeleteSupportTemplateView.as_view(), name='delete-template'),
+
+    # Message list view
+    path('templates/', SupportTemplatesView.as_view(), name='support-templates'),
+
+    # API endpoints
+    path('api/send-support/', SendSupportMessageView.as_view(), name='send-support'),
+    path('api/received-messages/', ReceivedSupportMessagesView.as_view(), name='received-messages'),
     path('api/notifications/', NotificationsView.as_view(), name='notifications'),
-    path("api/messageasread/", MessagesAsReadView.as_view(), name="message-as-read"),
+    path('api/messageasread/', MessagesAsReadView.as_view(), name='message-as-read'),
 ]
+
